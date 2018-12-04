fb3c83bdb912237e311e0c9733bb23fed83b823f
Pauline Garcin <paulinegarcin28@gmail.com>
2018-12-04 18:48:54 +0100

    profile show- sans css

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,10 @@
 class ProfilesController < ApplicationController
+
   def profile_params
     params.require(:user).permit(:first_name, :last_name, :photo)
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
