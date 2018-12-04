a0c758658fe43262d30e525c15b05c63c466113a
arthur d achon <arthur.dachon@gmail.com>
2018-12-04 11:12:48 +0100

    routes and controllers

---
+++ app/controllers/drivees_controller.rb
@@ -0,0 +1,2 @@
+class DriveesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :journeys do
+      resources :drivees, only: :new
+      resources :messages, only: [:new, :create]
+      resources :reviews, only: [:new, :create]
+    end
+    resources :profiles, only: [:show, :update]
+    resources :past_journeys, only: :index
 end
