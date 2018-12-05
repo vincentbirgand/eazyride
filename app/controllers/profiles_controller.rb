bb38c2734474158b9e9ccfd697798f2339c4f0d5
Pauline Garcin <paulinegarcin28@gmail.com>
2018-12-05 12:45:02 +0100

    profile en cours

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,19 +1,22 @@
 class ProfilesController < ApplicationController
-
-  def profile_params
-    params.require(:user).permit(:first_name, :last_name, :photo)
-  end
-
-  def new
-    @user = User.new
-  end
-
   def show
     @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = current_user
+  end
 
+  def update
+    @user = current_user
+    @user.update(user_params)
+    @user.save
+    redirect_to profile_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :description, :email, :phone, :address, :password, :birth_date, :gender, :verified, :photo)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     resources :journeys do
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
       resources :messages, only: [:new, :create]
       resources :reviews, only: [:new, :create]
     end
-    resources :profiles, only: [:show, :update]
+    resources :profiles, only: [:show, :edit, :update]
     resources :past_journeys, only: :index
 end
