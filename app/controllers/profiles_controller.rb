4f41622a622641790fd206cc91148771f8cafa10
Pauline Garcin <paulinegarcin28@gmail.com>
2018-12-05 10:42:43 +0100

    commit

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,7 +4,16 @@ class ProfilesController < ApplicationController
     params.require(:user).permit(:first_name, :last_name, :photo)
   end
 
+  def new
+    @user = User.new
+  end
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+
   end
 end
