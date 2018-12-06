01b788baad345028dc6f511078954f4ac1a24b06
arthur d achon <arthur.dachon@gmail.com>
2018-12-06 11:33:01 +0100

    Seeds corrigé avec Drivees et scénario sur Journey 1 + route Reviews + model activité adapté

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,12 +1,12 @@
 class Activity < ApplicationRecord
   has_many :journeys, dependent: :destroy
-  CATEGORY = ["Sport de neige", "Sport d'eau"].freeze
+  CATEGORY = ["Sport de neige", "Sport d'eau", "Sport de montagne"].freeze
   validates :category, inclusion: {
       in: CATEGORY,
 
     }
 
-  NAME = ["Ski", "Surf", "Snowboard", "Kite-Surf"].freeze
+  NAME = ["Ski", "Surf", "Snowboard", "Kite-Surf", "Escalade"].freeze
   validates :name, inclusion: {
       in: NAME,
     }
