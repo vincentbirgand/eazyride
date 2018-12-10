66012fd64830242a1eb80cc020d77d81c212409b
vincent birgand <birgandvincent@gmail.com>
2018-12-10 17:51:57 +0100

    seed1

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,22 +13,22 @@ Activity.destroy_all
 puts 'Cleaning database of users...'
 User.destroy_all
 
-puts 'generating 5 users with password 123456 '
+puts 'generating 10 users with password 123456 '
 
-vincent = User.create!(
+u1_vincent = User.create!(
   first_name: "Vincent",
   last_name: "Birgand",
-  description: "Passionné de kitesurf, je navigue régulièrement et dispose de plusieurs boards et ailes",
+  description: "Passionné de kitesurf, je navigue régulièrement et dispose de plusieurs boards et ailes. J'aime aussi faire du ski quand je suis en Rhône Alpes. Je dispose de mon matériel à Lyon.",
   phone: "07 09 09 09 09",
   birth_date: "28 Juillet 1988",
   gender: "man",
   verified: true,
   email: "vincent@eazyride.com",
   password: "123456",
-  address: "26 rue Edouard Vaillant, 93140 Bondy, France",
+  address: "16 villa Gaudelet, 75011 Paris, France"
  )
 
- arthur = User.create!(
+ u2_arthur = User.create!(
   first_name: "Arthur",
   last_name: "d'Achon",
   description: "Passionné de surf, je navigue régulièrement et dispose de plusieurs boards",
@@ -41,7 +41,7 @@ vincent = User.create!(
   address: "3 rue de la Pompe, 75016 Paris, France"
 )
 
-martin = User.create!(
+u3_martin = User.create!(
   first_name: "Martin",
   last_name: "Jordan",
   description: "Passionné de char à voile extrême, je vais régulièrement sur le sable afin de me confronter aux éléments et sentir le vent dans mes cheveux",
@@ -53,7 +53,7 @@ martin = User.create!(
   password: "123456",
   address: "6 rue Euryale Dehaynin, 75019 Paris, France"
 )
-thibaut = User.create!(
+u4_thibaut = User.create!(
   first_name: "Thibaut",
   last_name: "Duroute",
   description: "Passionné de ski de fond, je vais régulièrement sur les pistes et dispose de mon matériels",
@@ -63,9 +63,9 @@ thibaut = User.create!(
   verified: true,
   email: "thibault@eazyride.com",
   password: "123456",
-  address: "16 villa Gaudelet, 75011 Paris, France"
+  address: "26 rue Edouard Vaillant, 93140 Bondy, France"
 )
-pauline = User.create!(
+u5_pauline = User.create!(
   first_name: "Pauline",
   last_name: "Garcin",
   description: "Passionnée d'escalade, je vais régulièrement m'entrainer et dispose de mon matériel",
@@ -79,27 +79,113 @@ pauline = User.create!(
   # photo: 'https://www.climbing.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_620/MTQ4NTAxMDM1ODA2MTA2NzY1/o4bcgtff7lkuic44pxu4.jpg'
 )
 # pauline.remote_photo_url = 'https://www.climbing.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_620/MTQ4NTAxMDM1ODA2MTA2NzY1/o4bcgtff7lkuic44pxu4.jpg'
-pauline.save
-puts "5 users created"
+# pauline.save
 
-puts "creating 5 activities"
+u6_jean = User.create!(
+  first_name: "Jean",
+  last_name: "Charles",
+  description: "Passionné de kitesurf, je navigue régulièrement et dispose de plusieurs boards et ailes. J'aime aussi faire du ski quand je suis en Rhône Alpes. Je dispose de mon matériel à Lyon.",
+  phone: "07 09 19 09 09",
+  birth_date: "28 Juillet 1992",
+  gender: "man",
+  verified: true,
+  email: "jean@eazyride.com",
+  password: "123456",
+  address: "16 villa Gaudelet, 75011 Paris, France"
+ )
 
-a1 = Activity.create!(category:'Sport de neige', name:'Ski', photo: 'https://res.cloudinary.com/dscu7dmwa/image/upload/v1544450679/sport_de_neige.jpg')
-a2 = Activity.create!(category:'Sport de neige', name:'Snowboard', photo: 'https://res.cloudinary.com/dscu7dmwa/image/upload/v1544450679/sport_de_neige.jpg')
-a3 = Activity.create!(category:"Sport d'eau", name:'Surf', photo: 'https://res.cloudinary.com/dscu7dmwa/image/upload/v1544450832/ocean.jpg')
-a4 = Activity.create!(category:"Sport d'eau", name:'Kite-Surf', photo: 'https://res.cloudinary.com/dscu7dmwa/image/upload/v1544450832/ocean.jpg')
-a5 = Activity.create!(category:"Sport de montagne", name:'Escalade', photo: 'https://res.cloudinary.com/dscu7dmwa/image/upload/v1544450900/climbing.jpg')
+u7_peichan = User.create!(
+  first_name: "Peichan",
+  last_name: "Lee",
+  description: "Passionné de kitesurf, je navigue régulièrement et dispose de plusieurs boards et ailes. J'aime aussi faire du ski quand je suis en Rhône Alpes. Je dispose de mon matériel à Lyon.",
+  phone: "07 09 29 09 09",
+  birth_date: "28 Juillet 1992",
+  gender: "man",
+  verified: true,
+  email: "peichan@eazyride.com",
+  password: "123456",
+  address: "16 villa Gaudelet, 75011 Paris, France"
+ )
 
-puts "5 activities created"
+u8_paul = User.create!(
+  first_name: "Paul",
+  last_name: "Cotché",
+  description: "Passionné de kitesurf, je navigue régulièrement et dispose de plusieurs boards et ailes. J'aime aussi faire du ski quand je suis en Rhône Alpes. Je dispose de mon matériel à Lyon.",
+  phone: "07 09 39 09 09",
+  birth_date: "28 Juillet 1992",
+  gender: "man",
+  verified: true,
+  email: "paul@eazyride.com",
+  password: "123456",
+  address: "16 villa Gaudelet, 75011 Paris, France"
+ )
+
+u9_patrick = User.create!(
+  first_name: "Patrick",
+  last_name: "Fontaine",
+  description: "Passionné de kitesurf, je navigue régulièrement et dispose de plusieurs boards et ailes. J'aime aussi faire du ski quand je suis en Rhône Alpes. Je dispose de mon matériel à Lyon.",
+  phone: "07 09 49 09 09",
+  birth_date: "28 Juillet 1992",
+  gender: "man",
+  verified: true,
+  email: "patrick@eazyride.com",
+  password: "123456",
+  address: "16 villa Gaudelet, 75011 Paris, France"
+ )
+
+u10_julien = User.create!(
+  first_name: "Julien",
+  last_name: "Dupont",
+  description: "Passionné de kitesurf, je navigue régulièrement et dispose de plusieurs boards et ailes. J'aime aussi faire du ski quand je suis en Rhône Alpes. Je dispose de mon matériel à Lyon.",
+  phone: "07 09 59 09 09",
+  birth_date: "28 Juillet 1992",
+  gender: "man",
+  verified: true,
+  email: "julien@eazyride.com",
+  password: "123456",
+  address: "16 villa Gaudelet, 75011 Paris, France"
+ )
+
+
+
+
+puts "10 users created"
+
+puts "creating 12 activities"
+
+a1 = Activity.create!(category:"Plongée", name:'Apnée', photo: '')
+a2 = Activity.create!(category:"Sport d'eau", name:'Bodyboard', photo: '')
+a3 = Activity.create!(category:"Sport de montagne", name:'Escalade', photo: 'https://res.cloudinary.com/dscu7dmwa/image/upload/v1544450900/climbing.jpg')
+a4 = Activity.create!(category:"Golf", name:'Golf', photo: '')
+a5 = Activity.create!(category:"Sport d'eau", name:'Kite-Surf', photo: 'https://res.cloudinary.com/dscu7dmwa/image/upload/v1544450832/ocean.jpg')
+a6 = Activity.create!(category:"Sport d'eau", name:'Paddle', photo: '')
+a7 = Activity.create!(category:"Sport de montagne", name:'Parapente', photo: '')
+a8 = Activity.create!(category:"Sport de montagne", name:'Parachutisme', photo: '')
+a9 = Activity.create!(category:"Sport d'eau", name:'Planche à voile', photo: '')
+a10 = Activity.create!(category:"Plongée", name:'Plongée', photo: '')
+a11 = Activity.create!(category:"Randonnée", name:'Randonnée', photo: '')
+a12 = Activity.create!(category:"Running", name:'Running', photo: '')
+a13 = Activity.create!(category:'Sport de neige', name:'Ski', photo: 'https://res.cloudinary.com/dscu7dmwa/image/upload/v1544450679/sport_de_neige.jpg')
+a14 = Activity.create!(category:'Sport de neige', name:'Snowboard', photo: 'https://res.cloudinary.com/dscu7dmwa/image/upload/v1544450679/sport_de_neige.jpg')
+a15 = Activity.create!(category:"Sport d'eau", name:'Surf', photo: 'https://res.cloudinary.com/dscu7dmwa/image/upload/v1544450832/ocean.jpg')
+a16 = Activity.create!(category:"Vélo", name:'Vélo', photo: '')
+a17 = Activity.create!(category:"Vélo", name:'VTT', photo: '')
+
+
+
+
+
+
+puts "12 activities created"
 
 puts "creating 1 journey for each user"
 j1 = Journey.new(
   car_type: "Peugeot 5008",
   seat_available: 3,
-  description: "Hello les Eazyriders, je propose un voyage au départ de Lyon pour aller skier à la Clusaz le weekend prochain. Je recherche des gens bon esprit, contents de partager un bon moment de ride. A très vite!",
+  description: "Hello les Eazyriders, je propose un voyage au départ de Lyon pour aller skier à la Clusaz. Je recherche des gens bon esprit, contents de partager un bon moment de ride. A très vite!",
   source_city: "Lyon",
   destination_city: "La Clusaz",
-  price: 40,
+  price: 32,
   accepts_gear: true,
   shares_gear: true,
   lends_gear: true,
@@ -107,17 +193,17 @@ j1 = Journey.new(
   end_time: DateTime.new(2018, 12, 20, 18, 30)
 )
 
-j1.user_id = arthur.id
-j1.activity_id = a1.id
+j1.user_id = u2_arthur.id
+j1.activity_id = a13.id
 j1.save!
 
 j2 = Journey.new(
   car_type: "Porsche Boxter",
   seat_available: 3,
-  description: "Salut la communauté, je vous propose un voyage pour aller snowboarder samedi prochain à Méribel au départ de Lyon. J'ai des barres de toit donc de la place pour le matos volumineux. Bons skieurs voulus, préférence pour le hors-pistes. A très vite!",
+  description: "Salut la communauté, je vous propose un voyage pour aller ski samedi prochain à Méribel au départ de Lyon. J'ai des barres de toit donc de la place pour le matos volumineux. Bons skieurs voulus, préférence pour le hors-pistes. A très vite!",
   source_city: "Lyon",
   destination_city: "Les Arcs 2000",
-  price: 20,
+  price: 30,
   accepts_gear: true,
   shares_gear: true,
   lends_gear: true,
@@ -125,16 +211,16 @@ j2 = Journey.new(
   end_time: DateTime.new(2018, 12, 20, 18, 30)
 )
 
-j2.user_id = martin.id
-j2.activity_id = a2.id
+j2.user_id = u3_martin.id
+j2.activity_id = a13.id
 j2.save!
 
 j3 = Journey.new(
   car_type: "Volkswagen Polo",
   seat_available: 2,
-  description: "Hello, je proppose un voyage sur de Paris à Quiberon, n'hésitez pas à me PM pour plus d'infos. A toute!",
-  source_city: "Paris",
-  destination_city: "Quiberon",
+  description: "Hello, je proppose un voyage Lyon à la Station de Savoie Grand Revard, n'hésitez pas à me PM pour plus d'infos. A toute!",
+  source_city: "Lyon",
+  destination_city: "Station de Savoie Grand Revard",
   price: 40,
   accepts_gear: true,
   shares_gear: false,
@@ -143,17 +229,17 @@ j3 = Journey.new(
   end_time: DateTime.new(2018, 12, 20, 18, 30)
 )
 
-j3.user_id = vincent.id
-j3.activity_id = a3.id
+j3.user_id = u6_jean.id
+j3.activity_id = a13.id
 j3.save!
 
 j4 = Journey.new(
   car_type: "Volkswagen Combi",
   seat_available: 4,
-  description: "Voyage Kite-surf à saisir pour le Touquet au départ de Paris, je pars avec trois ailes donc ravi de partager mon matos, et j'ai de la place dans le coffre. A très vite!",
+  description: "Voyage ski à saisir pour Tignes au départ de Lyon, je pars avec trois paires de ski donc ravi de partager mon matos, et j'ai de la place dans le coffre. A très vite!",
   source_city: "Paris",
-  destination_city: "Le Touquet-Paris-Plage",
-  price: 24,
+  destination_city: "Tignes",
+  price: 34,
   accepts_gear: true,
   shares_gear: true,
   lends_gear: true,
@@ -161,8 +247,8 @@ j4 = Journey.new(
   end_time: DateTime.new(2018, 12, 20, 18, 30)
 )
 
-j4.user_id = thibaut.id
-j4.activity_id = a4.id
+j4.user_id = u4_thibaut.id
+j4.activity_id = a13.id
 j4.save!
 
 
@@ -172,6 +258,24 @@ j5 = Journey.new(
   description: "Je pars faire de l'escalade à Chamonix au départ de Lyon et j'ai deux places dans ma voiture. Je pars avec mon frère, nous recherchons des passionés d'escalade pour partager un bon moment!",
   source_city: "Lyon",
   destination_city: "Chamonix",
+  price: 23,
+  accepts_gear: true,
+  shares_gear: true,
+  lends_gear: true,
+  start_time: DateTime.new(2018, 12, 21, 9),
+  end_time: DateTime.new(2018, 12, 21, 18, 30)
+)
+
+j5.user_id = u5_pauline.id
+j5.activity_id = a3.id
+j5.save!
+
+j6 = Journey.new(
+  car_type: "clio 2",
+  seat_available: 3,
+  description: "Je pars faire de la plongée à Etel au départ de Rennes et j'ai deux places dans ma voiture. Je pars avec mon frère, nous recherchons des passionés de plongée pour partager un bon moment!. Nous plongerons avec le club d'Etel.",
+  source_city: "Rennes",
+  destination_city: "Etel",
   price: 13,
   accepts_gear: true,
   shares_gear: true,
@@ -180,9 +284,88 @@ j5 = Journey.new(
   end_time: DateTime.new(2018, 12, 21, 18, 30)
 )
 
-j5.user_id = pauline.id
-j5.activity_id = a5.id
-j5.save!
+j6.user_id = u6_jean.id
+j6.activity_id = a10.id
+j6.save!
+
+j7 = Journey.new(
+  car_type: "Audi RS4",
+  seat_available: 3,
+  description: "Salut la communauté, je vous propose un voyage pour aller surfer samedi prochain à Biarritz au départ de Bordeaux. J'ai des barres de toit donc de la place pour le matos volumineux. A très vite!",
+  source_city: "Bordeaux",
+  destination_city: "Biarritz",
+  price: 20,
+  accepts_gear: true,
+  shares_gear: true,
+  lends_gear: true,
+  start_time: DateTime.new(2018, 12, 15, 9),
+  end_time: DateTime.new(2018, 12, 15, 18, 30)
+)
+
+j7.user_id = u7_peichan.id
+j7.activity_id = a15.id
+j7.save!
+
+j8 = Journey.new(
+  car_type: "Renault Laguna",
+  seat_available: 2,
+  description: "Hello, je proppose un voyage sur de Paris à Quiberon, n'hésitez pas à me PM pour plus d'infos. A toute!",
+  source_city: "Paris",
+  destination_city: "Quiberon",
+  price: 50,
+  accepts_gear: true,
+  shares_gear: false,
+  lends_gear: false,
+  start_time: DateTime.new(2018, 12, 20, 5),
+  end_time: DateTime.new(2018, 12, 20, 18, 30)
+)
+
+j8.user_id = u8_paul.id
+j8.activity_id = a15.id
+j8.save!
+
+j9 = Journey.new(
+  car_type: "Volkswagen Combi",
+  seat_available: 4,
+  description: "Voyage Kite-surf à saisir pour le Touquet au départ de Paris, je pars avec trois ailes donc ravi de partager mon matos, et j'ai de la place dans le coffre. A très vite!",
+  source_city: "Paris",
+  destination_city: "Le Touquet-Paris-Plage",
+  price: 54,
+  accepts_gear: true,
+  shares_gear: true,
+  lends_gear: true,
+  start_time: DateTime.new(2018, 12, 20, 9),
+  end_time: DateTime.new(2018, 12, 20, 18, 30)
+)
+
+j9.user_id = u9_patrick.id
+j9.activity_id = a5.id
+j9.save!
+
+j10 = Journey.new(
+  car_type: "Renault Scenic",
+  seat_available: 4,
+  description: "Voyage Kite-surf à saisir pour le Kerhilio au départ de Paris, je pars avec 2 ailes donc ravi de partager mon matos, et j'ai de la place dans le coffre. A très vite!",
+  source_city: "Paris",
+  destination_city: "Kerhilio",
+  price: 60,
+  accepts_gear: true,
+  shares_gear: true,
+  lends_gear: true,
+  start_time: DateTime.new(2018, 12, 20, 6),
+  end_time: DateTime.new(2018, 12, 20, 18, 30)
+)
+
+j10.user_id = u10_julien.id
+j10.activity_id = a5.id
+j10.save!
+
+
+
+
+
+
+
 
 puts "5 journeys created"
 puts "creating 4 reviews for trip 1"
@@ -191,8 +374,8 @@ r1 = Review.new(
   rating: 5,
   description: "Super trip, Arthur est très sympa. Je le recommande vivement. A refaire!",
 )
-r1.reviewee_id = arthur.id
-r1.reviewer_id = vincent.id
+r1.reviewee_id = u2_arthur.id
+r1.reviewer_id = u1_vincent.id
 r1.journey_id = j1.id
 r1.save!
 
@@ -200,8 +383,8 @@ r2 = Review.new(
   rating: 1,
   description: "A fuir!!! Arthur est arrivé en retard, et ne s'est même pas excusé. Conduite désastreuse. Fuyez pour votre sécurité!",
 )
-r2.reviewee_id = arthur.id
-r2.reviewer_id = martin.id
+r2.reviewee_id = u2_arthur.id
+r2.reviewer_id = u3_martin.id
 r2.journey_id = j1.id
 r2.save!
 
@@ -209,8 +392,8 @@ r3 = Review.new(
   rating: 5,
   description: "Top! Le courant est vraiment bien passé avec Martin. Je pense qu'on repartira très vite rider ensemble, trop cool ;) !",
 )
-r3.reviewee_id = martin.id
-r3.reviewer_id = arthur.id
+r3.reviewee_id = u3_martin.id
+r3.reviewer_id = u2_arthur.id
 r3.journey_id = j1.id
 r3.save!
 
@@ -218,8 +401,8 @@ r4 = Review.new(
   rating: 4,
   description: "Vincent était en retard, ce qui nous a malheureusement fait tomber dans les bouchons. Mais il est très sympa et bon esprit. Je le recommande.",
 )
-r4.reviewee_id = vincent.id
-r4.reviewer_id = arthur.id
+r4.reviewee_id = u1_vincent.id
+r4.reviewer_id = u2_arthur.id
 r4.journey_id = j1.id
 r4.save!
 
@@ -228,12 +411,12 @@ puts " 2 drivees for journey 1"
 
 d1 = Drivee.new(status: "En attente")
 d1.journey_id = j1.id
-d1.user_id = vincent.id
+d1.user_id = u1_vincent.id
 d1.save!
 
 d2 = Drivee.new(status: "En attente")
 d2.journey_id = j1.id
-d2.user_id = martin.id
+d2.user_id = u3_martin.id
 d2.save!
 
 puts " 2 drivees created"
