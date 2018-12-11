c590afb17d0138ff796c2ffb36ec120cd66f1fb0
arthur d achon <arthur.dachon@gmail.com>
2018-12-11 15:32:10 +0100

    messagerie + pundit done

---
+++ app/policies/profile_policy.rb
@@ -0,0 +1,20 @@
+class ProfilePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+  def index?
+    return true
+  end
+  def show?
+    return true
+  end
+  def new?
+    create?
+  end
+  def create?
+    return true
+  end
+
+end
